204f956d679e4251a5b152bd42f7797684b6e6f5
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2025-01-13 20:27:02 -0500

    React web application with typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      Hello Santosh Reddy Bujala 👋
+      <span className="header"> Test</span>
+    </div>
+  );
+};
+
+export default App;
